4cfbc760e0a3dd4768534990d3ee469447f9584e
ashavelev <1992Samsung>
2016-04-11 14:25:30 +0200

    constant urls

--- apps/erlgram/include/settings.hrl
+++ apps/erlgram/include/settings.hrl
@@ -8,5 +8,5 @@
 %%%-------------------------------------------------------------------
 -author("alex_shavelev").
 
--define(BOT_TOKEN, <<"">>).
--define(TELEGRAM_URL, "").
+-define(BOT_TOKEN, "99434746:AAETpph94f6EIIBY-reY6CNoJp50nw-fdoQ").
+-define(TELEGRAM_URL, "https://api.telegram.org/").
